3a0f3386f62bd9f892a4a0b6703bff8676f4b847
Dinesh475 <dineshbalaji475@gmail.com>
2021-02-06 13:52:12 +0530

    1938

--- CALCULATING SP/src/index.js
+++ CALCULATING SP/src/index.js
@@ -3,7 +3,8 @@
 
 // Write a Function to return compute the doundof selling price.
 function find_SellingPrice(a,b){
-    
-    return null;
+    let lost_price = b/100*a;
+    let selling_price=Math.round(a-lost_price);
+    return selling_price;
 }
 //Use SpecRunner to check the Test Cases.
